59270401cee957040e289e0a22b6b8d318159964
JyotiradityaShukla <sukul.jyotir6455@gmail.com>
2024-05-09 15:07:55 +0530

    commit3

---
+++ 8-circle.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+
+    float rad;
+    printf("Enter radius of circle: ");
+    scanf("%f", &rad);
+
+    printf("Area of circle is %f", 3.14*rad*rad);
+    return 0;
+
+} 
---
+++ 9-rectangle.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main(){
+
+    float a,b;
+    printf("Enter length: ");
+    scanf("%f", &a);
+
+    printf("Enter breath: ");
+    scanf("%f", &b);
+
+    printf("Perimeter of rectangle is : %f", 2*a + 2*b);
+    return 0;
+
+} 
